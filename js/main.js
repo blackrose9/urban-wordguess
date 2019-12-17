0cb2360e0fe753cf939a0134f9aee8840833821e
blackrose9 <sarah.mwendwa@gmail.com>
2019-12-17 16:19:16 +0300

    Initial Commit. Defines one category of words to guess

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// words and hints
+var categoryOcean = [["Starfish","It should in the sky but it's in the sea"], ["Seahorse","It's land counterpart is great at races"], ["Water"],["Mizuuuu"], ["swordfish","Sharp, Sharp, pointy!"],["beach","Full of Sandiness"]];
+
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
